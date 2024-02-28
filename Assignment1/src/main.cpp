6c9506dcf86f23363d6ce96ec59e7978d2f42459
Luka de Vrij <lukadevrij@gmail.com>
2024-02-28 13:27:12 +0100

    button lcd combo

--- Assignment1/src/main.cpp
+++ Assignment1/src/main.cpp
@@ -1,22 +1,59 @@
 #include <Arduino.h>
+#include <LiquidCrystal.h>
 
 // put function declarations here:
 int myFunction(int, int);
 
-void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+// initialize the library by associating any needed LCD interface pin
+// with the arduino pin number it is connected to
+const int rs = 9, en = 8, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+// constants won't change. They're used here to set pin numbers:
+const int buttonPin = 0; // the number of the pushbutton pin
+const int ledPin = 13;   // the number of the LED pin
+
+int buttonPresses = 0;
+int currentlyPressed = false;
+// variables will change:
+int buttonState = 0; // variable for reading the pushbutton status
+
+void setup()
+{
+  // set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
+
+  pinMode(ledPin, OUTPUT);
+  // initialize the pushbutton pin as an input:
+  pinMode(buttonPin, INPUT);
 }
 
-// the loop function runs over and over again forever
-void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
-  delay(100);                      // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
-  delay(10000);                      // wait for a second
-}
+void loop()
+{
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  // read the state of the pushbutton value:
+  buttonState = digitalRead(buttonPin);
+
+  // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
+  if (buttonState == HIGH)
+  {
+    // turn LED on:
+    currentlyPressed = true;
+    digitalWrite(ledPin, LOW);
+  }
+  else
+  {
+    // turn LED off:
+    if (currentlyPressed == true)
+    {
+      buttonPresses++;
+      currentlyPressed = false;
+    }
+
+    digitalWrite(ledPin, HIGH);
+  }
+  // set the cursor to column 0, line 1
+  // (note: line 1 is the second row, since counting begins with 0):
+  lcd.setCursor(0, 1);
+  // print the number of seconds since reset:
+  lcd.print(buttonPresses);
 }
