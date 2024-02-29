7e41b0a77dd9d7308e91788a307a99470127846b
Luka de Vrij <lukadevrij@gmail.com>
2024-02-29 23:17:34 +0100

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- Assignment1/src/main.cpp
+++ Assignment1/src/main.cpp
@@ -3,7 +3,8 @@
 
 // put function declarations here:
 void pin0Press();
-void startOpMode();
+void pin1Press();
+void printOpMenu();
 void scrollInOpMode();
 void opModeSelection();
 
@@ -13,16 +14,19 @@ const int rs = 9, en = 8, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 // constants won't change. They're used here to set pin numbers:
 const int button0Pin = 0; // the number of the pushbutton pin
+const int button1Pin = 1; // the number of the pushbutton pin
 const int ledPin = 13;	  // the number of the LED pin
 int button0PressCount = 0;
 int button0Pressed = false;
-
+int button1Pressed = false;
 // variables will change:
-volatile int buttonState = 0; // variable for reading the pushbutton status
+volatile int button0State = 0; // variable for reading the pushbutton status
+volatile int button1State = 0; // variable for reading the pushbutton status
 
 // Op Mode
 int opMode = false;
-int opModeCursor = 0;
+unsigned int opModeCursor = 0;
+char opMenuLines[5][16] = {"Do A", "Do B > 60", "Go Back", "-"};
 
 // typedef enum
 // {
@@ -34,6 +38,7 @@ int opModeCursor = 0;
 // 	STATE_TRIGGERED,
 // } State;
 
+
 void setup()
 {
 	Serial.begin(9600);
@@ -43,7 +48,10 @@ void setup()
 	// initialize the pushbutton pin as an input:
 	pinMode(button0Pin, INPUT);
 
-	attachInterrupt(0, pin0Press, CHANGE);
+
+	// i dont get interrupts: we zouden alleen op pin 2 en 3 kunnen interrupten; maar heb toch echt pin 0 geintewrrupt?
+	attachInterrupt(0, pin0Press, CHANGE); 
+	attachInterrupt(digitalPinToInterrupt(1), pin1Press, CHANGE);
 }
 
 void loop()
@@ -56,47 +64,91 @@ void pin0Press()
 	// Check if OP mode - then pin 0 is SELECT
 
 	// read the state of the pushbutton value:
-	buttonState = digitalRead(button0Pin);
-	Serial.println(buttonState);
+	button0State = digitalRead(button0Pin);
 
-	if (buttonState == LOW) // means button is pressed NOW
+	if (button0State == LOW) // means button is pressed NOW
 	{
 		if (!button0Pressed) // if not already pressed down
 		{
 			// this is only called once, as soon as button is pressed!
-			
+
 			button0Pressed = true;
 			digitalWrite(ledPin, HIGH);
-			if (opMode) {
+			if (opMode)
+			{
+				Serial.println("SE");
 				opModeSelection(); // this function must do something based on opModeCursor (not yet implemented)
 			};
 			opMode = true;
-			startOpMode();
+			printOpMenu();
 		}
 	}
 	else // means button is not pressed
 	{
 		digitalWrite(ledPin, LOW);
+		button0Pressed = false;
 	}
-	// set the cursor to column 0, line 1
-	// (note: line 1 is the second row, since counting begins with 0):
-	lcd.setCursor(0, 1);
-	// print the number of seconds since reset:
-	lcd.print(button0PressCount);
+
+	lcd.setCursor(0, 1); // No clue why this is here but if we remove it everything crashes?!
 }
 
-void startOpMode()
+void pin1Press()
 {
-	lcd.setCursor(0,0);
-	lcd.print("Do A");
-	lcd.setCursor(0,1);
-	lcd.print("Do B > 60");
+	// read the state of the pushbutton value:
+	button1State = digitalRead(button1Pin);
+
+	if (button1State == LOW) // means button is pressed NOW
+	{
+
+		if (!button1Pressed) // if not already pressed down
+		{
+			// this is only called once, as soon as button is pressed!
+			Serial.println("b1");
+			button1Pressed = true;
+			digitalWrite(ledPin, HIGH);
+			if (opMode)
+			{
+				
+				scrollInOpMode();
+			};
+		}
+	}
+	else
+	{
+		digitalWrite(ledPin, HIGH);
+		button1Pressed = false;
+	}
+}
+
+void printOpMenu()
+{
+
+	lcd.setCursor(0, 0);
+	lcd.clear();
+	lcd.print(">");
+	lcd.print(opMenuLines[opModeCursor]);
+
+	if (opModeCursor < sizeof(opMenuLines))
+	{
+		lcd.setCursor(0, 1);
+		lcd.print(" ");
+		lcd.print(opMenuLines[opModeCursor + 1]);
+	}
 }
 
 void scrollInOpMode()
 {
+	opModeCursor++;
+	printOpMenu();
+	Serial.println("SC");
 }
 
-void opModeSelection(){
+void opModeSelection()
+{
 
+	// niet de beste manier om erachter te komen op welke optie je staat maar anders moeten we mappen en ram is schaars
+	Serial.println(opModeCursor);
+	// switch(){
+
+	// }
 }
