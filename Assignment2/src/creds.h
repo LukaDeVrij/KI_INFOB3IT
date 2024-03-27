d890681a958157bdac8b1be0fb538b8e56d3c48f
Imbert-Dam <imbert@casema.nl>
2024-03-27 13:01:12 +0100

    integration

--- Assignment2/src/creds.h
+++ Assignment2/src/creds.h
@@ -1,6 +1,6 @@
-const char* ssid = "";
-const char* password = "";
+const char* ssid = "WiFilesshome";
+const char* password = "Toegang321";
 const char* mqtt_server = "science-vs352.science.uu.nl";
 const char* mqtt_username = "student018";
-const char* mqtt_password = "";
+const char* mqtt_password = "W2hyG5H3";
 // 
--- Assignment2/src/main.cpp
+++ Assignment2/src/main.cpp
@@ -26,6 +26,7 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 #include <Adafruit_BMP280.h>
+#include <Servo.h>
 
 #define SCREEN_WIDTH 128    // OLED display width, in pixels
 #define SCREEN_HEIGHT 64    // OLED display height, in pixels
@@ -40,130 +41,31 @@ const int analogInputPin = A0;
 const int servoPin = D5;
 const int buttonPin = D7;
 
+const int rodAngle = 90;
+
 WiFiClient espClient;
 PubSubClient client(espClient);
+Servo myServo;
+
 unsigned long lastMsg = 0;
 unsigned long lastRefresh = 0;
+unsigned long lastSoil = 0;
+unsigned long lastWater = 0;
+bool soilDelay = false;
+bool waterDelay = false;
+
 #define MSG_BUFFER_SIZE (50)
 char msg[MSG_BUFFER_SIZE];
 int value = 0;
 
-void setup()
-{
-    Serial.begin(9600);
-
-    // Setup pin modes:
-    pinMode(selectPin, OUTPUT);
-    pinMode(analogInputPin, INPUT);
-    pinMode(buttonPin, INPUT);
-    pinMode(LED_BUILTIN, OUTPUT); // Initialize the BUILTIN_LED pin as an output
-
-    digitalWrite(LED_BUILTIN, HIGH); // Set LED off by default
-
-    setup_hardware();
-
-    // MQTT
-    setup_wifi();
-    client.setServer(mqtt_server, 1883);
-    client.setCallback(callback); // Callback is called when anything is received
-}
-
-void loop()
-{
-
-    mqtt_loop();
-    hardware_loop();
-}
-
-void mqtt_loop()
-{
-    if (!client.connected())
-    {
-        reconnect();
-    }
-    client.loop();
-
-    unsigned long now = millis();
-    if (now - lastMsg > 10000)
-    {
-        lastMsg = now;
-        ++value;
-        snprintf(msg, MSG_BUFFER_SIZE, "hello world #%ld", value);
-        Serial.print("Publish message: ");
-        Serial.println(msg);
-        client.publish("infob3it/student018/outTopic", msg);
-    }
-}
-
-void hardware_loop()
-{
-    unsigned long now = millis();
-    if (now - lastRefresh > 2000)
-    {
-        lastRefresh = now;
-
-        Serial.print(F("Temperature = "));
-        float temp = bmp.readTemperature();
-        Serial.print(temp);
-        Serial.println(" *C");
-
-        display.print(temp);
-
-        Serial.print(F("Pressure = "));
-        Serial.print(bmp.readPressure());
-        Serial.println(" Pa");
-
-        Serial.print(F("Approx altitude = "));
-        Serial.print(bmp.readAltitude(1013.25)); /* Adjusted to local forecast! */
-        Serial.println(" m");
-
-        Serial.println();
-    }
-}
-
-void setup_wifi()
-{
-
-    delay(10);
-    // We start by connecting to a WiFi network
-    Serial.println();
-    Serial.print("Connecting to ");
-    Serial.println(ssid);
-
-    WiFi.mode(WIFI_STA);
-    WiFi.begin(ssid, password);
-
-    while (WiFi.status() != WL_CONNECTED)
-    {
-        delay(500);
-        Serial.print(".");
-    }
-
-    randomSeed(micros());
-
-    Serial.println("");
-    Serial.println("WiFi connected");
-    Serial.println("IP address: ");
-    Serial.println(WiFi.localIP());
-}
-
-void setup_hardware()
-{
-    // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
-    if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS))
-    {
-        Serial.println(F("SSD1306 allocation failed"));
-    }
-    if (!bmp.begin(BMP280_ADDRESS_ALT, BMP280_CHIPID))
-    {
-        Serial.println(F("BM280 allocation failed"));
-    }
-
-    display.setTextSize(2);
-    display.setTextColor(SSD1306_WHITE);
-    display.setCursor(0,0);
-
-}
+// Method initialisation
+void setup_hardware();
+void setup_wifi();
+void setup_hardware();
+// void callback();
+void mqtt_loop();
+void hardware_loop();
+// void reconnect();
 
 void callback(char *topic, byte *payload, unsigned int length)
 {
@@ -217,3 +119,146 @@ void reconnect()
         }
     }
 }
+
+
+void setup()
+{
+    Serial.begin(9600);
+
+    // Setup pin modes:
+    pinMode(selectPin, OUTPUT);
+    pinMode(analogInputPin, INPUT);
+    pinMode(buttonPin, INPUT);
+    pinMode(LED_BUILTIN, OUTPUT); // Initialize the BUILTIN_LED pin as an output
+
+    digitalWrite(LED_BUILTIN, HIGH); // Set LED off by default
+
+    setup_hardware();
+
+    // MQTT
+    setup_wifi();
+    client.setServer(mqtt_server, 1883);
+    client.setCallback(callback); // Callback is called when anything is received
+}
+
+void loop()
+{
+
+    mqtt_loop();
+    hardware_loop();
+}
+
+void mqtt_loop()
+{
+    if (!client.connected())
+    {
+        reconnect();
+    }
+    client.loop();
+
+    unsigned long now = millis();
+    if (now - lastMsg > 10000)
+    {
+        lastMsg = now;
+        ++value;
+        snprintf(msg, MSG_BUFFER_SIZE, "hello world #%ld", value);
+        Serial.print("Publish message: ");
+        Serial.println(msg);
+        client.publish("infob3it/student018/outTopic", msg);
+    }
+}
+
+void hardware_loop()
+{
+
+    unsigned long now = millis();
+    if (now - lastRefresh > 2000)
+    {
+        display.clearDisplay();
+        display.setCursor(0,0);
+
+        lastRefresh = now;
+
+        float temp = bmp.readTemperature();
+        float pressure = bmp.readPressure();
+        display.println(temp);
+        display.println(pressure);
+        display.display();
+        if(!soilDelay)
+        {
+            Serial.println(analogRead(analogInputPin));
+        }
+    }
+    if(soilDelay && now - lastSoil > 100)
+    {
+        soilDelay = false;
+        lastSoil = now;
+        Serial.println(analogRead(analogInputPin));
+        digitalWrite(selectPin,LOW);
+        
+        //plaats vervangend voor button / methode TODO
+        waterDelay = true;
+        lastWater = now;
+        myServo.attach(servoPin);
+        myServo.write(rodAngle);
+
+    }
+    
+    if(now - lastSoil > 10000)
+    {
+        digitalWrite(selectPin,HIGH);
+        soilDelay = true;
+        lastSoil = now;
+    }
+    if(waterDelay && now - lastWater > 1000)
+    {
+        waterDelay = false;
+        myServo.write(0);
+        myServo.detach();
+    }
+    
+}
+
+void setup_wifi()
+{
+
+    delay(10);
+    // We start by connecting to a WiFi network
+    Serial.println();
+    Serial.print("Connecting to ");
+    Serial.println(ssid);
+
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid, password);
+
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        delay(500);
+        Serial.print(".");
+    }
+
+    randomSeed(micros());
+
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+}
+
+void setup_hardware()
+{
+    // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+    if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS))
+    {
+        Serial.println(F("SSD1306 allocation failed"));
+    }
+    if (!bmp.begin(BMP280_ADDRESS_ALT, BMP280_CHIPID))
+    {
+        Serial.println(F("BM280 allocation failed"));
+    }
+
+    display.setTextSize(2);
+    display.setTextColor(SSD1306_WHITE);
+
+}
+
