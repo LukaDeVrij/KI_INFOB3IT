5178b4dafd58f170bbe9342a4a30fc4cb4a68401
Luka de Vrij <lukadevrij@gmail.com>
2024-03-27 10:28:29 +0100

    Delete creds.h

--- Assignment2/src/creds.h
+++
@@ -1,5 +0,0 @@
-const char* ssid = "";
-const char* password = "";
-const char* mqtt_server = "science-vs352.science.uu.nl";
-const char* mqtt_username = "student018";
-const char* mqtt_password = "";
